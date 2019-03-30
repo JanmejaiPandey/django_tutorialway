ffa1596e002e48ff820268a873775bb5ddfe8962
JanmejaiPandey <pandey.janmejai@gmail.com>
2019-03-30 12:19:15 +0530

    Added filesss

--- tutorialhero/settings.py
+++ tutorialhero/settings.py
@@ -115,6 +115,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+#heroku
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
