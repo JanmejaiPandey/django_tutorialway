34f11ef69a5d4ab2ce4e9e5299a33454ceab6b22
JanmejaiPandey <pandey.janmejai@gmail.com>
2019-04-04 20:51:25 +0530

    menu2

--- tutorialhero/settings.py
+++ tutorialhero/settings.py
@@ -33,8 +33,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
-    'djangosecure',
-    'sslserver',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
