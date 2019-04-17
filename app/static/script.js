992c1b0633bb8efda653e228e1bc623b5428d4d6
JanmejaiPandey <pandey.janmejai@gmail.com>
2019-04-17 12:53:27 +0530

    pageread

--- app/static/script.js
+++ app/static/script.js
@@ -1,15 +1,29 @@
-$(document).ready(function(){     
+$(document).ready(function(){ 
+$(function(){
+  $('.toggle-menu').click(function(e){
+    e.preventDefault();
+    $('.sidebar').toggleClass('toggled');
+  });
+});    
   var w = $(window).height();     
   var width = 1;
   var p=0;
   var d = $(document).height();
   var elem = document.getElementById("myPageBar");
   var scrolled_val = $(document).scrollTop();
-  elem.style.width = ((w+scrolled_val)/d)*100 +'%';
+  var percent_initial = ((w+scrolled_val)/d)*100; 
+  elem.style.width = percent_initial +'%'; 
+  $("#perc").html("PageRead:<br>"+percent_initial+"%");
+ 
   $(window).scroll( function() { 
+    
     var scrolled_val = $(document).scrollTop();
     var sum = w+scrolled_val;
     var percent = (sum/d)*100;
+    if(percent <= 100)
+    {
+    $("#perc").html("PageRead:<br>"+percent+"%");
+    }
     elem.style.width = percent +'%';
     });
   });
