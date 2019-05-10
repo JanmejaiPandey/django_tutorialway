1015c38166d970a52ccf2f4b361fd56fc594d0b1
JanmejaiPandey <48506722+JanmejaiPandey@users.noreply.github.com>
2019-05-10 16:34:40 +0530

    logot changed size

--- tutorialhero/settings/production.py
+++ tutorialhero/settings/production.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR =os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -103,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# django_heroku.settings(locals())
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
